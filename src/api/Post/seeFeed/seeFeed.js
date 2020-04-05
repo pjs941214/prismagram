1bbeb793927dce1375d8cbb73633f6b050e29a16
jaewon <pjs941214@naver.com>
2020-04-05 22:17:46 +0900

    seeFeed Resolver

---
+++ src/api/Post/seeFeed/seeFeed.js
@@ -0,0 +1,25 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+	Query: {
+		seeFeed: async (_, __, { request, isAuthenticated }) => {
+			isAuthenticated(request);
+			const { user } = request;
+			const following = await prisma
+				.user({ id: user.id })
+				.following();
+			return prisma.posts({
+				where: {
+					user: {
+						id_in: [
+							...following
+								.map(user => user.id)
+								.push(user.id)
+						]
+					}
+				},
+				orderBy: "createdAt_DESC"
+			});
+		}
+	}
+};
