6654a267959bcde0b2dc4c9ab49707acd11ed2ca
jaewon <pjs941214@naver.com>
2020-04-06 21:19:02 +0900

    seeRoom Resolver

--- src/fragments.js
+++ src/fragments.js
@@ -1,12 +1,24 @@
 export const USER_FRAGMENT = `
     id
     username
+    avatar
 `;
 
 export const COMMENT_FRAGMENT = `
     id
     text
     user {
+        ${USER_FRAGMENT}
+    }
+`;
+
+export const MESSAGE_FRAGMENT = `
+    id
+    text
+    to {
+        ${USER_FRAGMENT}
+    }
+    from {
         ${USER_FRAGMENT}
     }
 `;
@@ -29,6 +41,18 @@ export const FULL_POST_FRAGMENT = `
         }
         user {
             ${USER_FRAGMENT}
+        }
+    }
+`;
+
+export const ROOM_FRAGMENT = `
+    fragment RoomParts on Room {
+        id
+        participants {
+            ${USER_FRAGMENT}
+        }
+        messages { 
+            ${MESSAGE_FRAGMENT}
         }
     }
 `;
